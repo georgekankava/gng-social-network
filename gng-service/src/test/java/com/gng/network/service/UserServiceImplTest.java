c44b5632c07a9912e0bff9d735573f45d9454e21
George Kankava <george.kankava@aurea.com>
2016-11-24 12:33:41 +0400

    Adding test coverages for service class.

--- gng-service/src/test/java/com/gng/network/service/UserServiceImplTest.java
+++ gng-service/src/test/java/com/gng/network/service/UserServiceImplTest.java
@@ -34,6 +34,7 @@ public class UserServiceImplTest {
     private String username = "testusername";
     private String password = "testpassword";
     private String lastname = "testLastName";
+    private String fullname = "Test Test";
 
     @Mock
     private UserDao userDao;
@@ -56,14 +57,60 @@ public class UserServiceImplTest {
 
     @Before
     public void before() {
+        User userById = mock(User.class);
+        User user1 = mock(User.class);
+        User user2 = mock(User.class);
+        User user3 = mock(User.class);
+        when(user1.getId()).thenReturn(1);
+        when(user2.getId()).thenReturn(2);
+        when(user3.getId()).thenReturn(3);
+        when(user1.getUsername()).thenReturn("user1");
+        when(user2.getUsername()).thenReturn("user2");
+        when(user3.getUsername()).thenReturn("user3");
+        when(user.getFriends()).thenReturn(Arrays.asList(user1, user2, user3));
+        when(userDao.mergeUser(user)).thenReturn(user);
+        when(userById.getId()).thenReturn(23);
+        when(userById.getFullname()).thenReturn(fullname);
+        when(userDao.findUserById(23)).thenReturn(userById);
+        when(userDao.findUserByFullname(fullname)).thenReturn(users);
         when(helper.convertUsersToJsonString(userResponseJson)).thenReturn("{}");
         when(helper.convertUsersToJsonUsers(users)).thenReturn(userResponseJson);
         when(userDao.searchUsersBySearchString("testSearchString")).thenReturn(users);
         when(userDao.findUserByUsername(username)).thenReturn(user);
-        when(userDao.findUsersByLastname(lastname)).thenReturn(Arrays.asList(new User(), new User()));
-        when(userDao.findUsersByFirstname(username)).thenReturn(Arrays.asList(new User(), new User()));
+        when(userDao.findUsersByLastname(lastname)).thenReturn(users);
+        when(userDao.findUsersByFirstname(username)).thenReturn(users);
         when(userDao.findUserByUsername(username)).thenReturn(user);
         when(user.getPassword()).thenReturn(password);
+    }
+
+    @Test
+    public void testGetUsersFriends() {
+        List<User> userFriends = userService.getUsersFriends(user);
+        assertThat(userFriends.size()).isEqualTo(3);
+        assertThat(userFriends.get(0).getUsername()).isEqualTo("user1");
+        assertThat(userFriends.get(1).getUsername()).isEqualTo("user2");
+        assertThat(userFriends.get(2).getUsername()).isEqualTo("user3");
+        verify(userDao, atLeastOnce()).mergeUser(user);
+    }
+
+    @Test
+    public void testGetUsersFriendIds() {
+        List<Integer> userFriendsIds = userService.getUsersFriendUserIds(user);
+        assertThat(userFriendsIds.size()).isEqualTo(3);
+        assertThat(userFriendsIds.get(0)).isEqualTo(1);
+        assertThat(userFriendsIds.get(1)).isEqualTo(2);
+        assertThat(userFriendsIds.get(2)).isEqualTo(3);
+        verify(userDao, times(1)).mergeUser(user);
+    }
+
+    @Test
+    public void testGetUsersFriendUsernames() {
+        List<String> userFriendsUsernames = userService.getUsersFriendUsernames(user);
+        assertThat(userFriendsUsernames.size()).isEqualTo(3);
+        assertThat(userFriendsUsernames.get(0)).isEqualTo("user1");
+        assertThat(userFriendsUsernames.get(1)).isEqualTo("user2");
+        assertThat(userFriendsUsernames.get(2)).isEqualTo("user3");
+        verify(userDao, times(1)).mergeUser(user);
     }
 
     @Test(expected = UserNotFoundException.class)
@@ -92,6 +139,25 @@ public class UserServiceImplTest {
         verify(userDao, atLeastOnce()).searchUsersBySearchString("testSearchString");
         verify(helper, times(1)).convertUsersToJsonUsers(users);
         verify(helper, times(1)).convertUsersToJsonString(userResponseJson);
+
+        List<User> usersByFullname = userService.findUserByFullname(fullname);
+        assertThat(usersByFullname).isNotEmpty();
+        verify(userDao, times(1)).findUserByFullname(fullname);
+
+        User userById = userService.findUserById(23);
+        assertThat(userById).isNotNull();
+        assertThat(userById.getId()).isEqualTo(23);
+        assertThat(userById.getFullname()).isEqualTo(fullname);
+        verify(userDao, atLeast(1)).findUserById(23);
+
+        try {
+            userService.findUserById(24);
+        } catch(UserNotFoundException e) {
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+
+        userService.updateUser(this.user);
+        verify(userDao, times(1)).updateUser(this.user);
     }
 
     @Test
